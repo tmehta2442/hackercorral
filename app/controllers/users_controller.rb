8896cd7d9945b09d557c42a50be11aa14988aea9
tmehta2442 <tmehta@post.harvard.edu>
2013-12-10 18:36:18 -0800

    yay, authentication all done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to root_url, notice: "Thanks for signing up"
 		else 
 			render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Hackercorral::Application.routes.draw do
+
+	get 'signup', to: 'users#new', as: 'signup'
+	get 'signin', to: 'sessions#new', as: 'signin'
+	get 'signout', to: 'sessions#destroy', as: 'signout'
+
   root :to => 'users#index'
   # get "users/about"
   get "site/about"
   get "site/help"
 
   resources :users
+  resources :sessions
   # resources :site
-
-
+  
 end
