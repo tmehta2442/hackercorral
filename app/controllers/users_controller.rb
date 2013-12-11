f464f8530c5f3f3bc539035271496430d53b238e
tmehta2442 <tmehta@post.harvard.edu>
2013-12-10 17:02:04 -0800

    starting authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
 
 	def index
+		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to root_url, notice: "Thanks for signing up"
+		else 
+			render "new"
+		end
 	end
 
 end
