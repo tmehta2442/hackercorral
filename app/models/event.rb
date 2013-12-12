4ecaa22a3f3d63274a56d914ba1f09bdc45189d8
tmehta2442 <tmehta@post.harvard.edu>
2013-12-12 12:43:57 -0800

    this should get events working on heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 	
-	attr_accessible :event_name, :event_location, :event_details
+	attr_accessible :event_name, :event_location, :event_details, :user_id
 
   
 end
