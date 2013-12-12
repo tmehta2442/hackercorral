9494b95b1feb24ce0e4a71165b49d48e379453fb
tmehta2442 <tmehta@post.harvard.edu>
2013-12-11 19:29:14 -0800

    starting on events

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Hackercorral::Application.routes.draw do
 
-	get 'signup', to: 'users#new', as: 'signup'
-	get 'signin', to: 'sessions#new', as: 'signin'
+	get 'signup',  to: 'users#new', as: 'signup'
+	get 'signin',  to: 'sessions#new', as: 'signin'
 	get 'signout', to: 'sessions#destroy', as: 'signout'
-
+  get 'event',   to: 'events#index', as: 'event'
+  
   root :to => 'users#index'
   # get "users/about"
   get "site/about"
@@ -11,6 +12,7 @@ Hackercorral::Application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :events
   # resources :site
   
 end
