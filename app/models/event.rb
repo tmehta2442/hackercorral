48def59d58ba5e497e9d7a7a17b41870a41cbef0
tmehta2442 <tmehta@post.harvard.edu>
2013-12-12 20:33:21 -0800

    CALENDAR yay, and date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ActiveRecord::Base
 	belongs_to :user
 	
-	attr_accessible :event_name, :event_location, :event_details, :user_id
+	attr_accessible :event_name, :event_location, :event_details, 
+									:user_id, :start_time
 
   
 end
