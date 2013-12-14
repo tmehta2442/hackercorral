d9607bd9c55f8a36b4daece06d66191b7ac8042a
tmehta2442 <tmehta@post.harvard.edu>
2013-12-14 14:59:13 -0800

    modified js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,31 +1,46 @@
 	    
 $(function(){
 	var map;
-  var markers = [];                             // Where all markers are stored
-  var currentLat;                               // These two used to create pothole with last position
+  var markers = [];
+  var currentLat;
   var currentLng;
-  var infobox = new google.maps.InfoWindow({    // One infobox, only content changes
+  var infobox = new google.maps.InfoWindow({
      content: "Hello!",
      boxStyle:{
       border: "1px solid black"
-     }
-  });
-  var isWindowOpen = false;                     // Used so only one window opens at a time
-  var newMarkerExists = false;                  // Used so only one marker creaed at a time
-  var blueDot = "/assets/blueDot.png";
-  var currentMarker; 
+  }
+});
 
+var isWindowOpen = false;
+var newMarkerExists = false;
+var blueDot = "/assets/blueDot.png";
+var currentMarker;
 
-
-	function initialize() {
-	  var mapOptions = {
-	    center: new google.maps.LatLng(37.7833, -122.4167),
-	    zoom: 12,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDoubleClickZoom: true,
-      styles: [{"featureType": "administrative", "stylers": [{"visibility": "off"} ] }, {"featureType": "poi", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "water", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "transit", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "landscape", "stylers": [{"visibility": "simplified"} ] }, {"featureType": "road.highway", "stylers": [{"visibility": "off"} ] }, {"featureType": "road.local", "stylers": [{"visibility": "on"} ] }, {"featureType": "road.highway", "elementType": "geometry", "stylers": [{"visibility": "on"} ] }, {"featureType": "water", "stylers": [{"color": "#84afa3"}, {"lightness": 52 } ] }, {"stylers": [{"saturation": -17 }, {"gamma": 0.36 } ] }, {"featureType": "transit.line", "elementType": "geometry", "stylers": [{"color": "#3f518c"} ] } ] };
-	  var map = new google.maps.Map(document.getElementById("map-canvas"),
-	      mapOptions);
+function initialize() {
+	var mapOptions = {
+    center: new google.maps.LatLng(37.7833, -122.4167),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDoubleClickZoom: true,
+    styles: [{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]}] 
+  	};
+  	map = new google.maps.Map(document.getElementById("map-canvas"),
+  		mapOptions);
 	}
-	google.maps.event.addDomListener(window, 'load', initialize);
+		google.maps.event.addDomListener(window, 'load', initialize);
+		
+		if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      var currentPosition = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	      var userMarker = new google.maps.Marker({
+	        position: currentPosition,
+	        map: map,
+	        icon: blueDot
+	      });
+      map.setCenter(currentPosition);
+    }, 
+  	function() {
+      handleNoGeolocation(true);
+    });
+  }
 });
