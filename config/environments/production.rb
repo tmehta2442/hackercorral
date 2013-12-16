086cdbce84b561168c8c90f05c91c3ab15aee171
tmehta2442 <tmehta@post.harvard.edu>
2013-12-15 18:49:38 -0800

    oh, forgot production stuffs for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,17 @@ Hackercorral::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "hackercorral.com",
+    authentication: "plain",
+    enable_starttls_auth: true,
+    user_name: "hackercorral",
+    password: "WDIProject3"
+  }
+
 
   # Enable threaded mode
   # config.threadsafe!
