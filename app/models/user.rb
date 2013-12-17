4e918981c54d8d341f3c40272e359b8e9836e5c7
tmehta2442 <tmehta@post.harvard.edu>
2013-12-16 22:15:03 -0800

    added username, fixed navbar, email working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
 
   has_many :events
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation,
+  								:username
 
-  validates_uniqueness_of :email
+  # validates_uniqueness_of :username, presence: true, length : { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_uniqueness_of :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131213034520) do
+ActiveRecord::Schema.define(:version => 20131217054038) do
 
   create_table "events", :force => true do |t|
     t.string   "event_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20131213034520) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "username"
   end
 
 end
