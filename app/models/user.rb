4d2b5d82d60d58fc08aea89f2202800dd5062a2f
tmehta2442 <tmehta@post.harvard.edu>
2013-12-30 17:54:08 -0800

    remember me feature working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :joined_events, through: :event_participants,
   				 source: :event
-  	
+  
+  before_create { generate_token(:auth_token) }
 
   attr_accessible :email, :password, :password_confirmation,
   								:username
@@ -14,4 +15,10 @@ class User < ActiveRecord::Base
   # validates_uniqueness_of :username, presence: true, length : { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_uniqueness_of :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131218011028) do
+ActiveRecord::Schema.define(:version => 20131231013430) do
 
   create_table "event_participants", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20131218011028) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "username"
+    t.string   "auth_token"
   end
 
 end
